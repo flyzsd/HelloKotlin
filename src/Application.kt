341318402b3c065bfc01b44985071addf58afe97
Shudong Zhang <zhangshudong@gmail.com>
2019-09-12 23:20:13 +0800

    Add code

--- src/Application.kt
+++ src/Application.kt
@@ -1,3 +1,73 @@
-fun main(){
+fun main() {
     println("hello world!")
+    numbers()
+    string()
+}
+
+fun numbers() {
+    val number1 = 123
+    val number2 = 0x0001_03EF
+    val number3 = 0b1011
+    val oneMillion = 1_000_000
+    val number4: Int? = 456
+    val number5 = number3.toInt()
+    val char1 = 'a'
+    val char2 = '\uFF00';
+    var string2 = "😇"
+
+    if (number3 in number1..number2) {
+    }
+
+    for (element in string2) {
+        println(element.toInt())
+    }
+}
+
+fun array() {
+    val array1 = arrayOf(1, 2, 3)
+    val array2 = arrayOfNulls<Int>(5)
+    val array3 = Array(5) { it.toString() }
+    val array4 = intArrayOf(4, 5, 6)
+    val array5 = LongArray(7)
+    val array6 = ByteArray(10)
+    for ((index, value) in array1.withIndex()) {
+        println("the element at $index is $value")
+    }
+}
+
+fun string() {
+    val text1 = """
+    for (c in "foo")
+        print(c)
+    """.trimIndent()
+
+    val text2 = """
+    |Tell me and I forget.
+    |Teach me and I remember.
+    |Involve me and I learn.
+    |(Benjamin Franklin)
+    """.trimMargin()
+    println(text1)
+
+    val s = "abc"
+    println("$s.length is ${s.length}")
+}
+
+fun maxOfTwo(a: Int, b: Int) {
+    val max = if (a > b) {
+        print("Choose a")
+        a
+    } else {
+        print("Choose b")
+        b
+    }
+}
+
+fun hasPrefix(x: Any) : Unit = when (x) {
+    1, 2 -> println("is 1 or 2")
+    in 3 until 10 -> println("in 3 until 10")
+    !in 20..100 -> println("not in 20..100")
+    is String -> println("is String")
+    !is Int -> println("is not Int")
+    else -> println("else")
 }
